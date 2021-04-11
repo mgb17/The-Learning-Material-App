040f957a648c73a4cac245a0dfb0ac868d32a40d
M. Görkem <63784397+mgb17@users.noreply.github.com>
2021-04-11 21:55:38 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <ul>
+        <LearningResource v-for="res in storedResources" :key="res.id"
+        :title="res.title" :description="res.description" :link="res.link" />
+    </ul>
+  
+</template>
+
+<script>
+
+import LearningResource from './components/learning-resources/LearningResource';
+
+export default {
+    components: {
+        LearningResource
+    },
+    data() {
+        return {
+            storedResources: [
+                { 
+                  id: 'official-guide',
+                  title: 'Official Guide',
+                  description: 'The official Vue.js documentation',
+                  link: 'https://vuejs.org'
+                },
+                { 
+                  id: 'google',
+                  title: 'Google',
+                  description: 'How to google to learn effectively',
+                  link: 'https://google.com'
+                },
+            ]
+        };
+    }
+}
+</script>
