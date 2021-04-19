d46630883201bd21d94dcb0f8219ffe3675abd31
Mert <63784397+mgb17@users.noreply.github.com>
2021-04-19 23:31:06 +0200

    add resource

--- src/components/learning-resources/StoredResources.vue
+++ src/components/learning-resources/StoredResources.vue
@@ -13,7 +13,6 @@ export default {
         LearningResource
     },
     inject: ['resources']
-        
     }
 </script>
 
--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -7,7 +7,9 @@
                     :mode="addResButtonMode"
                     >Add Resource</base-button>
     </base-card>
-    <component :is="selectedTab"></component>
+    <keep-alive>
+      <component :is="selectedTab"></component>
+    </keep-alive>
 </template>
 
 <script>
@@ -40,7 +42,8 @@ export default {
     },
     provide() {
         return {
-            resources: this.storedResources
+            resources: this.storedResources,
+            addResource: this.addResource
         };
     },
     computed: {
@@ -54,6 +57,16 @@ export default {
     methods: {
         setSelectedTab(tab) {
             this.selectedTab = tab;
+        },
+        addResource(title, description, url) {
+            const newResource = {
+                id: new Date().toISOString(),
+                title: title,
+                description: description,
+                link: url
+            };
+            this.storedResources.unshift(newResource);
+            this.selectedTab = 'stored-resources';
         }
     }
 }
